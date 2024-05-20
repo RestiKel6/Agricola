3811f05854a1d15140d6ce0501cace37a61bb7d1
ZevonikRF <rifky.fachrizain@gmail.com>
2024-05-20 20:45:47 +0700

    update home

--- app/index.tsx
+++ app/index.tsx
@@ -14,10 +14,14 @@ export default function Index() {
       <View style={styles.body}>
         <View style={styles.boxContainer}>
           <View style={styles.yellowBox}>
+            <Image source={require('../assets/images/location-icon.png')} style={styles.locationIcon}/>
             <Text style={styles.yellowBoxText}>Jatinangor, Indonesia</Text>
           </View>
-          <View style={styles.grayBox} />
+        </View>
+        <View style={styles.lowerBody}>
           <Text style={styles.bodyText}>Kondisi Perkebunan</Text>
+          <View style={styles.grayBoxSuhu}></View>
+          <View style={styles.grayBoxHumid}></View>
         </View>
       </View>
     </View>
@@ -50,22 +54,22 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     paddingRight: 40,
-    marginRight: 40,
+    marginRight: 50,
     marginTop: 20,
   },
   body: {
     flex: 4,
     backgroundColor: '#EBE3D5',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    //justifyContent: 'flex-start',
+    //alignItems: 'center',
   },
   bodyContent: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   boxContainer: {
-    justifyContent: 'center',
+    //justifyContent: 'center',
     alignItems: 'center',
     marginTop: 20,
   },
@@ -75,23 +79,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fec459',
     borderRadius: 8,
     marginBottom: 10, 
-    justifyContent: 'center',
+    //justifyContent: 'center',
+    flexDirection: 'row',
+    paddingTop: 13,
     paddingLeft: 10,
   },
-  grayBox: {
-    width: 300,
-    height: 170,
-    backgroundColor: '#d9d9d9',
-    borderRadius: 8,
-    marginBottom: 20, 
-  },
-  bodyText: {
-    fontSize: 16,
-    color: '#000',
-    textAlign: 'center',
+  locationIcon: {
+    // klo mulai ngaco gws
   },
   yellowBoxText: {
     fontSize: 16,
     color: '#000',
+    marginLeft: 10,
+  },
+  bodyText: {
+    fontSize: 16,
+    color: '#000',
+    //justifyContent: 'center'
+  },
+  lowerBody:{
+    marginTop: 15,
+    marginLeft: 50,
+  },
+  grayBoxSuhu: {
+    width: 300,
+    height: 150,
+    backgroundColor: '#d9d9d9',
+    borderRadius: 8,
+    marginTop: 15,
+  },
+  grayBoxHumid: {
+    width: 300,
+    height: 150,
+    backgroundColor: '#d9d9d9',
+    borderRadius: 8,
+    marginTop: 50,
+    marginLeft: 'auto',
+    marginRight: 50
   },
 });

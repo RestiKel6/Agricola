f862a35bb0f154ae149eb72b5a4b0cfaabdb6feb
mabismaramadhan <18221028@std.stei.itb.ac.id>
2024-05-20 22:22:17 +0700

    update lg

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
   return (

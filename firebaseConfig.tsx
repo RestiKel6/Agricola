401c5b4eacc4b4dbcb3ad7a64b51d889f1c5c2b6
Muhammad Mumtaz <18221029@mahasiswa.itb.ac.id>
2024-06-01 14:10:49 +0700

    feat: adding firebase & nodemcu code

---
+++ firebaseConfig.tsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFLX87OpekA2XO-F5gX2LRhZ9bkzMOHwk",
+  authDomain: "agricola-aa38f.firebaseapp.com",
+  databaseURL: "https://agricola-aa38f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "agricola-aa38f",
+  storageBucket: "agricola-aa38f.appspot.com",
+  messagingSenderId: "730144733230",
+  appId: "1:730144733230:web:4db60588a08dc64c0a07bf",
+  measurementId: "G-Z4BLJXGX6W"
+};
+
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

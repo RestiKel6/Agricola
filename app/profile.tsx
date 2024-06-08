305e2f1fdbf560fa68b34f813a4825ac46cdaa2f
ZevonikRF <rifky.fachrizain@gmail.com>
2024-06-08 19:01:57 +0700

    buat page profile

---
+++ app/profile.tsx
@@ -0,0 +1,149 @@
+import { useRouter } from "expo-router";
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+export default function Profile(){
+    const router = useRouter();
+    return(
+        <View style={styles.container}>
+            <View style={styles.upperContent}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.textWelcome}>Selamat Datang {"\n"}Petani Modern!</Text>
+                    </View>
+                    <View style={styles.boxLocation}>
+                        <View style={styles.boxContent}>
+                            <Image source={require("../assets/images/location-icon.png")}/>
+                            <Text style={styles.locationText}>Jatinangor, Indonesia</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.middleContent}>
+                <View style={styles.profileTextContainer}>
+                    {/* Nanti di fetch dari firebase */}
+                    <Text style={styles.profileNameText}>Name</Text>
+                    <Text style={styles.profileEmailText}>name@email.com</Text>
+                </View>
+            </View>
+            <View style={styles.profilePicContainer}>
+                <View style={styles.profilePic}>
+
+                </View>
+            </View>
+            <View style={styles.bottomNav}>
+                <View style={styles.bottomNavContent}>
+                    <TouchableOpacity onPress={() => router.push("/")}>
+                        <Image
+                            source={require("../assets/images/home.png")}
+                            style={styles.homeIcon}
+                        />
+                    </TouchableOpacity>
+                    <Image
+                        source={require("../assets/images/profil2.png")}
+                        style={styles.profileIcon}
+                    />
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#797e2a",
+    },
+    upperContent:{
+        flex: 1,
+        backgroundColor: "#797e2a",
+        alignItems: 'center',
+    },
+    boxContainer:{
+        width: '70%',
+        alignItems: 'center',
+    },
+    textContainer:{
+        marginTop: 50,
+        marginBottom: 25,
+        alignSelf: 'flex-start',
+    },
+    textWelcome:{
+        fontSize: 27,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    },
+
+    boxLocation:{
+        width: '100%',
+        height: 50,
+        backgroundColor: "#fec459",
+        borderRadius: 20,
+        justifyContent: 'center',
+    },
+
+    boxContent:{
+        flexDirection: 'row',
+        marginLeft: 10,
+        //justifyContent: 'center',
+        alignItems: 'center',
+    },
+    locationText:{
+        fontSize: 20,
+        marginLeft: 10,
+        fontWeight: 'bold',
+    },
+    middleContent:{
+        backgroundColor: "#ffffff",
+        width: '100%',
+        height: 550,
+        zIndex: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    profileTextContainer:{
+        marginTop: 100,
+        alignItems: 'center',
+    },
+
+    profileNameText:{
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    profileEmailText:{
+        fontSize: 15,
+    },
+    profilePicContainer:{
+        alignItems: 'center',
+        zIndex: 2,
+        position: 'absolute',
+        transform: [{ translateX: -60 }, { translateY: -60 }],
+        top: '30%',
+        left: '46%',
+    },
+    profilePic:{
+        backgroundColor: "#0f0f0f",
+        width: 170,
+        height: 170,
+        borderRadius: 120,
+    },
+    bottomNav:{
+        //flex: 1,
+        backgroundColor: "#d9d9d9",
+        height: 70,
+    },
+    bottomNavContent:{
+        flexDirection: 'row',
+        //justifyContent: 'center',
+        marginTop: 15,
+    },
+    homeIcon:{
+        marginRight:'auto',
+        marginLeft: 150,
+        //marginTop: 20,
+    },
+    profileIcon:{
+        marginLeft:'auto',
+        marginRight: 130,
+    },
+});
